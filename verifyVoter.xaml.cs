99dfbb623b3d10350c9af69af8e408226eba2ae6
jack-debug <55571956+jack-debug@users.noreply.github.com>
2020-11-21 17:14:51 +1100

    Add files via upload

---
+++ verifyVoter.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace eVote2020
+{
+    /// <summary>
+    /// Interaction logic for verifyVoter.xaml
+    /// </summary>
+    public partial class verifyVoter : Window
+    {
+        public verifyVoter()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            votePres tt = new votePres();
+            tt.Show();
+            this.Close();
+        }
+    }
+}
---
+++ votePres.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace eVote2020
+{
+    /// <summary>
+    /// Interaction logic for votePres.xaml
+    /// </summary>
+    public partial class votePres : Window
+    {
+        public votePres()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (dT.IsChecked == true)
+            {
+                MessageBox.Show("Thank you, your vote for Donald Trump has been recorded.");
+                System.Windows.Application.Current.Shutdown();
+            }
+            else if (jB.IsChecked == true)
+            {
+                MessageBox.Show("Thank you, your vote for Joe Biden has been recorded.");
+                System.Windows.Application.Current.Shutdown();
+            }
+            else if (jJ.IsChecked == true)
+            {
+                MessageBox.Show("Thank you, your vote for Jo Jorgensen has been recorded.");
+                System.Windows.Application.Current.Shutdown();
+            }
+            else if (hH.IsChecked == true)
+            {
+                MessageBox.Show("Thank you, your vote for Howie Hankins has been recorded.");
+                System.Windows.Application.Current.Shutdown();
+            }
+        }
+    }
+}
